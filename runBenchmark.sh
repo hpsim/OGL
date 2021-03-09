4b0a6df6d071808323317ce8a0c6b6d8dfd18eb0
Gregor Olenik <gregor.olenik@web.de>
2021-03-09 10:27:41 +0100

    use dnsFoam as benchmake case

--- runBenchmark.sh
+++ runBenchmark.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+ #!/bin/sh
 
 # Sets a solver 
 setSolver(){
@@ -11,28 +11,30 @@ setSolver(){
 
 # Derive a case from base case
 deriveCase(){
+    baseCase=boxTurbBase
     baseFolder=$PWD
     echo "Deriving case " $1 $2
     mkdir -p $1
-    cp -r pitzDailyBase $1/$2
+    cp -r $baseCase $1/$2
     cd $baseFolder
 }
 
 setCells() {
     echo "Setting Cells $1"
-    sed -i "s/180/$1/g" system/blockMeshDict
+    sed -i "s/16 16 16/$1 $1 $1/g" system/blockMeshDict
 }
 
 # Copy Pitzdaily base case
 generateBaseCase() {
-    FILE=pitzDailyBase/system/controlDict
+    baseCase=boxTurbBase
+    FILE=$baseCase/system/controlDict
     baseFolder=$PWD
     if [ ! -f "$FILE" ]; then
         echo "Preparing base case"
-        cp -r $FOAM_TUTORIALS/incompressible/pisoFoam/LES/pitzDaily/ pitzDailyBase
+        cp -r $FOAM_TUTORIALS/DNS/dnsFoam/boxTurb16  $baseCase
 
         echo "Meshing base case"
-        cd pitzDailyBase
+        cd $baseCase
         setCells $1
         blockMesh > blockMesh.log
 
@@ -42,7 +44,8 @@ generateBaseCase() {
         touch system/controlDict.foam
 
         # run only for 100 time steps
-        sed -i 's/endTime[ ]*0.1/endTime 1e-3/g' system/controlDict
+        endTime=0.1
+        sed -i "s/endTime[ ]*[0-9.]*/endTime $endTime/g" system/controlDict
 
         # clear p solver settings
         sed -z 's/p\n[ ]*[{][^}]*[}]/p{}/g' system/fvSolution > system/fvSolutionTMP
@@ -51,10 +54,11 @@ generateBaseCase() {
 }
 
 runSolver() {
+    solver=dnsFoam
     baseFolder=$PWD
     cd $1
     echo "Starting solver" $baseFolder
-    pisoFoam | tee solver.log | grep "Time ="
+    $solver | tee solver.log | grep "Time ="
     tail -n 20 solver.log | grep "ExecutionTime" > performance.log
     cd $baseFolder
 }
@@ -81,7 +85,7 @@ basePGKOCGOMP(){
 }
 
 basePGKOCUDA(){
-    solver="GKOCGOMP"
+    solver="GKOCGCUDA"
     deriveCase "p" "$solver"
     setSolver  "p/$solver" "p{solver GKOCG;tolerance 1e-06; relTol 0.0;smoother none;preconditioner none;maxIter 10000; app_executor omp; executor cuda;}"
     runSolver "p/$solver"
@@ -93,9 +97,9 @@ generatePerformanceLog(){
     timestamp=$(date +%s)
 
     lscpu | grep "Modelname" > stats/stats$timestamp.txt
-    lspci | grep "VGA" >> stats/stats%timestamp.txt
-    git log | head -n 1 >>  stats/stats%timestamp.txt
-    find Tests -name "performance.log" -exec echo {} \; -exec cat {}  \;
+    lspci | grep "VGA" >> stats/stats$timestamp.txt
+    git log | head -n 1 >>  stats/stats$timestamp.txt
+    find . -name "performance.log" -exec echo {} \; -exec cat {} >> stats/stats$timestamp.txt  \;
 }
 
 # run symmetric solver
@@ -103,7 +107,7 @@ mkdir -p Tests
 
 cd Tests
 
-for number in 10 50 100 200
+for number in 8 16 32 64 128
 do
 echo $number
     mkdir -p $number
@@ -113,7 +117,7 @@ echo $number
     basePGKOCGOMP
     basePOFCG
     basePGKOCG
-    basePGKOCUDA
+    # basePGKOCUDA
 
     generatePerformanceLog
 
