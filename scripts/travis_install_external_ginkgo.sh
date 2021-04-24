5ae35b80cff7cb8d4e4404af0a8a1fce75317463
greole <gregor.olenik@web.de>
2021-04-24 11:33:43 +0200

    Travis external ginkgo (#9)
    * add external ginkgo flag to ci/cd matrix
    
    * fix ci/cd matrix + test conditional job
    
    * add a test script for external ginkgo build
    
    * fix external ginkgo build flags to speedup build
    
    * Add missing sudo to install ginkgo to system.
    
    * fix cmake build arguments

---
+++ scripts/travis_install_external_ginkgo.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+if [ $EXTERNAL_GINKGO = ON ]
+then
+    git clone https://github.com/ginkgo-project/ginkgo.git
+    mkdir ginkgo/build
+    cd ginkgo/build
+    cmake \
+        -DGINKGO_BUILD_BENCHMARKS=OFF \
+        -DGINKGO_BUILD_EXAMPLES=OFF \
+        -DGINKGO_BUILD_CUDA=off \
+        -DGINKGO_BUILD_HIP=off \
+        -DGINKGO_BUILD_OMP=off \
+        -DGINKGO_BUILD_TESTS=off\
+        -DGINKGO_BUILD_REFERENCE=on \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DGINKGO_BUILD_HWLOC=off \
+        ..
+    make  && sudo make install
+fi
