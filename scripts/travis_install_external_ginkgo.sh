8330976341325bdf18caccc6f4928e0b3e2557e4
Gregor Olenik <gregor.olenik@web.de>
2021-05-10 09:30:01 +0200

    build current develop and a specific (last known to work) version of ginkgo

--- scripts/travis_install_external_ginkgo.sh
+++ scripts/travis_install_external_ginkgo.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-
 if [ $EXTERNAL_GINKGO = ON ]
 then
     git clone https://github.com/ginkgo-project/ginkgo.git
     mkdir ginkgo/build
     cd ginkgo/build
+    git checkout $GINKGO_VERSION
     cmake \
         -DGINKGO_BUILD_BENCHMARKS=OFF \
         -DGINKGO_BUILD_EXAMPLES=OFF \
