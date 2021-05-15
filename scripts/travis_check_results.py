5f0b20e950fd76a4e2fee2fc59176b9ef6137b43
Gregor Olenik <gregor.olenik@web.de>
2021-05-15 09:42:24 +0200

    Add script to validate test runs

---
+++ scripts/travis_check_results.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import sys
+
+
+def extract_iters_from_line(line, pos):
+    return float(line.split(",")[pos])
+
+
+def relative_difference(res1, res2):
+    return abs(res1 - res2) / res1
+
+
+if __name__ == "__main__":
+
+    fn = sys.argv[1]
+    with open(fn) as fh:
+        lines = fh.readlines()[1:]
+        for i in range(int(len(lines) / 2)):
+            rel_diff = relative_difference(
+                extract_iters_from_line(lines[2 * i], -1),
+                extract_iters_from_line(lines[2 * i + 1], -1),
+            )
+            if rel_diff > 0.01:
+                sys.exit(1)
