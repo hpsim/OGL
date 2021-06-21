afe874cff67dcea23ea46dec132bb8b5706bf420
Gregor Olenik <gregor.olenik@web.de>
2021-06-21 08:14:37 +0200

    Add timestep to matrix name when exporting

--- common/common.C
+++ common/common.C
@@ -10,19 +10,19 @@
 namespace Foam {
 
 void export_system(const word fieldName, const mtx *A, const vec *x,
-                   const vec *b)
+                   const vec *b, const word time)
 {
-    std::string fn_mtx{fieldName + "_A.mtx"};
+    std::string fn_mtx{time + "_" + fieldName + "_A.mtx"};
     std::ofstream stream{fn_mtx};
     std::cerr << "Writing " << fn_mtx << std::endl;
     gko::write(stream, A, gko::layout_type::coordinate);
 
-    std::string fn_b{fieldName + "_b.mtx"};
+    std::string fn_b{time + "_" + fieldName + "_b.mtx"};
     std::ofstream stream_b{fn_b};
     std::cerr << "Writing " << fn_b << std::endl;
     gko::write(stream_b, b);
 
-    std::string fn_x{fieldName + "_x.mtx"};
+    std::string fn_x{time + "_" + fieldName + "_x0.mtx"};
     std::ofstream stream_x{fn_x};
     std::cerr << "Writing " << fn_x << std::endl;
     gko::write(stream_x, x);
