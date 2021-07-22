f9d3b55b587bc336ed0090d1e03695efbd8d6b61
Gregor Olenik <gregor.olenik@web.de>
2021-07-22 10:47:15 +0200

    check iteration count

--- scripts/travis_check_results.py
+++ scripts/travis_check_results.py
@@ -17,10 +17,11 @@ if __name__ == "__main__":
         # TODO the first and second line have to be skipped
         # the starting line should be found automatically
         lines = fh.readlines()[2:]
+        position_iters_in_log = -3
         for i in range(int(len(lines) / 2)):
             rel_diff = relative_difference(
-                extract_iters_from_line(lines[2 * i], -1),
-                extract_iters_from_line(lines[2 * i + 1], -1),
+                extract_iters_from_line(lines[2 * i], position_iters_in_log),
+                extract_iters_from_line(lines[2 * i + 1], position_iters_in_log),
             )
             if rel_diff > 0.05:
                 sys.exit(1)
