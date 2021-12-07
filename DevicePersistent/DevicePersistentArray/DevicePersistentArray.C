36de5dd0866a5eaa5e13c838c7eaed43f38e165b
Gregor Olenik <go@hpsim.de>
2021-12-07 16:14:43 +0100

    Fix return type of get_db, add defineTemplate

--- DevicePersistent/DevicePersistentArray/DevicePersistentArray.C
+++ DevicePersistent/DevicePersistentArray/DevicePersistentArray.C
@@ -1,3 +1,10 @@
 #include "DevicePersistentArray.H"
 
-namespace Foam {}  // namespace Foam
+namespace Foam {
+
+
+// to store the std::shared_ptr<T> in the IO registry the type needs to be
+// declared
+defineTemplateTypeNameWithName(DevicePersistentBase<gko::Array<scalar>>,
+                               "PersistenScalarArray");
+}  // namespace Foam
