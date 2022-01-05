41a4f9ebe1295ffa17a57fcb16717e49f4980fe7
Gregor Olenik <go@hpsim.de>
2022-01-05 12:47:48 +0100

    Clean up

--- DevicePersistent/CsrMatrixWrapper/CsrMatrixWrapper.C
+++ DevicePersistent/CsrMatrixWrapper/CsrMatrixWrapper.C
@@ -92,11 +92,6 @@ std::shared_ptr<gko::matrix::Csr<scalar>> CsrInitFunctor::init() const
         auto values = values_.get_array();
         auto cols = col_idxs_.get_array();
         auto rows = row_idxs_.get_array();
-        for (int i = 0; i < values->get_num_elems(); i++) {
-            std::cout << "(" << rows->get_const_data()[i] << ","
-                      << cols->get_const_data()[i] << ") "
-                      << values->get_const_data()[i] << std::endl;
-        }
 
         auto coo_mtx = gko::share(coo_mtx::create(
             device_exec, gko::dim<2>(nCells, nCells),
