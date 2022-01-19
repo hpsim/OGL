42f38cb80138a4002465f2e0075cbbeb3c302c3e
Gregor Olenik <go@hpsim.de>
2022-01-19 11:15:05 +0100

    Add GMRES wrapper

---
+++ Solver/GMRES/GKOGMRES.C
@@ -0,0 +1,39 @@
+/*---------------------------------------------------------------------------*\
+License
+    This file is part of OGL.
+
+    OGL is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OGL is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OGL.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include <ginkgo/ginkgo.hpp>
+#include <map>
+#include <type_traits>
+#include "GKOGMRES.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam {
+
+defineTypeNameAndDebug(GKOGMRES, 0);
+
+lduMatrix::solver::addsymMatrixConstructorToTable<GKOGMRES>
+    addGKOGMRESSymMatrixConstructorToTable_;
+}  // namespace Foam
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+
+// ************************************************************************* //
