2ba2333b4148179ebf7e928c43497885bb4c7658
Gregor Olenik <go@hpsim.de>
2022-01-27 15:43:03 +0100

    Allow caching of generated preconditioner

--- DevicePersistent/DevicePersistentArray/DevicePersistentArray.C
+++ DevicePersistent/DevicePersistentArray/DevicePersistentArray.C
@@ -1,17 +1,3 @@
 #include "DevicePersistentArray.H"
 
-namespace Foam {
-
-// to store the std::shared_ptr<T> in the IO registry the type needs to be
-// declared
-defineTemplateTypeNameWithName(DevicePersistentBase<gko::matrix::Csr<scalar>>,
-                               "PersistentCSRMatrix");
-defineTemplateTypeNameWithName(DevicePersistentBase<gko::Array<scalar>>,
-                               "PersistentScalarArray");
-defineTemplateTypeNameWithName(DevicePersistentBase<gko::Array<label>>,
-                               "PersistentLabelArray");
-defineTemplateTypeNameWithName(DevicePersistentBase<gko::matrix::Dense<label>>,
-                               "PersistentLabelVec");
-defineTemplateTypeNameWithName(DevicePersistentBase<gko::matrix::Dense<scalar>>,
-                               "PersistentScalarVec");
-}  // namespace Foam
+namespace Foam {}  // namespace Foam
