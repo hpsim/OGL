c884d169d72e35ef30cf4c0fed56ba8f3c42f2a9
Gregor Olenik <go@hpsim.de>
2022-02-17 10:53:04 +0100

    Add read_distributed for matrix A

--- DevicePersistent/CsrMatrixWrapper/CsrMatrixWrapper.C
+++ DevicePersistent/CsrMatrixWrapper/CsrMatrixWrapper.C
@@ -61,10 +61,13 @@ std::shared_ptr<gko::matrix::Csr<scalar>> CsrInitFunctor::init() const
         word msg{"init global csr matrix of size " + std::to_string(nCells)};
         LOG_1(verbose_, msg)
 
-        auto values = values_.get_global_array();
-        auto cols = col_idxs_.get_global_array();
-        auto rows = row_idxs_.get_global_array();
+        // auto values = values_.get_global_array();
+        // auto cols = col_idxs_.get_global_array();
+        // auto rows = row_idxs_.get_global_array();
 
+        auto values = values_.get_array();
+        auto cols = col_idxs_.get_array();
+        auto rows = row_idxs_.get_array();
         // check if sorted
         if (false) {
             bool is_sorted_rows = true;
@@ -101,6 +104,17 @@ std::shared_ptr<gko::matrix::Csr<scalar>> CsrInitFunctor::init() const
         }
 
 
+        auto coo_mtx = gko::share(coo_mtx::create(
+            device_exec, gko::dim<2>(nCells, nCells),
+            val_array(device_exec, *values.get()), *cols.get(), *rows.get()));
+
+        auto gkomatrix =
+            gko::share(mtx::create(device_exec, gko::dim<2>(nCells, nCells)));
+        SIMPLE_TIME(verbose_, convert_coo_to_csr,
+                    coo_mtx->convert_to(gkomatrix.get());)
+        return gkomatrix;
+
+        // TODO FIXME
         if (Pstream::master()) {
             // for (int i = 0; i < values->get_num_elems(); i++) {
             //     std::cout << "(" << rows->get_const_data()[i] << ","
