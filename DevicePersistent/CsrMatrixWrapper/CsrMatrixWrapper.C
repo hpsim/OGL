0104e15a91f1037d14d6870fab966ce7159f8f45
Gregor Olenik <gregor.olenik@web.de>
2022-09-21 15:19:28 +0200

    Refactor DevicePersitent Matrix

--- DevicePersistent/CsrMatrixWrapper/CsrMatrixWrapper.C
+++ DevicePersistent/CsrMatrixWrapper/CsrMatrixWrapper.C
@@ -30,128 +30,102 @@ SourceFiles
 
 namespace Foam {
 
-void CsrInitFunctor::update(
-    std::shared_ptr<gko::matrix::Csr<scalar>> &csr_matrix) const
-{
-    auto values_view =
-        val_array::view(values_.get_exec_handler().get_device_exec(),
-                        values_.get_global_size(), csr_matrix->get_values());
-    // copy values to device
-    values_view = *values_.get_array().get();
-}
+// using dist_mtx = gko::distributed::Matrix<scalar, label, label>;
 
-std::shared_ptr<gko::matrix::Csr<scalar>> CsrInitFunctor::init() const
-{
-    using coo_mtx = gko::matrix::Coo<scalar>;
-
-    if (Pstream::parRun()) {
-        const auto device_exec = values_.get_exec_handler().get_device_exec();
-        const label nCells = global_index_.size();
-        word msg{"init global csr matrix of size " + std::to_string(nCells)};
-        LOG_1(verbose_, msg)
-
-        // auto values = values_.get_global_array();
-        // auto cols = col_idxs_.get_global_array();
-        // auto rows = row_idxs_.get_global_array();
-
-        auto values = values_.get_array();
-        auto cols = col_idxs_.get_array();
-        auto rows = row_idxs_.get_array();
-        // check if sorted
-        if (false) {
-            bool is_sorted_rows = true;
-            bool is_sorted_cols = true;
-            auto rows_data = rows->get_const_data();
-            auto cols_data = cols->get_const_data();
-            for (size_t i = 1; i < cols->get_num_elems(); i++) {
-                if (rows_data[i] < rows_data[i - 1]) {
-                    is_sorted_rows = false;
-                    Info << "rows sorting error element " << i << " row[i] "
-                         << rows_data[i] << " row[i-1] " << rows_data[i - 1]
-                         << endl;
-                }
-                // same row but subsequent column is smaller
-                if (cols_data[i] < cols_data[i - 1] &&
-                    rows_data[i] == rows_data[i - 1]) {
-                    is_sorted_cols = false;
-                    Info << "cols sorting error element " << i << " row[i] "
-                         << rows_data[i] << " row[i-1] " << rows_data[i - 1]
-                         << " col[i] " << cols_data[i] << " col[i-1] "
-                         << cols_data[i - 1] << endl;
-                }
-            }
-
-            Info << "is_sorted rows " << is_sorted_rows << endl;
-            Info << "is_sorted cols " << is_sorted_cols << endl;
-
-            if (!is_sorted_cols || !is_sorted_rows) {
-                for (size_t i = 1; i < cols->get_num_elems(); i++) {
-                    Info << i << "sparsity (" << rows_data[i] << ","
-                         << cols_data[i] << ")\n";
-                }
-            }
-        }
-
-
-        auto coo_mtx = gko::share(coo_mtx::create(
-            device_exec, gko::dim<2>(nCells, nCells),
-            val_array(device_exec, *values.get()), *cols.get(), *rows.get()));
-
-        auto gkomatrix =
-            gko::share(mtx::create(device_exec, gko::dim<2>(nCells, nCells)));
-        SIMPLE_TIME(verbose_, convert_coo_to_csr,
-                    coo_mtx->convert_to(gkomatrix.get());)
-        return gkomatrix;
-    } else {
-        const auto device_exec = values_.get_exec_handler().get_device_exec();
-        const label nCells = global_index_.size();
-        word msg{"init csr matrix of size " + std::to_string(nCells)};
-        LOG_1(verbose_, msg)
-        auto values = values_.get_array();
-        auto cols = col_idxs_.get_array();
-        auto rows = row_idxs_.get_array();
-
-        auto coo_mtx = gko::share(coo_mtx::create(
-            device_exec, gko::dim<2>(nCells, nCells),
-            val_array(device_exec, *values_.get_array().get()),
-            *col_idxs_.get_array().get(), *row_idxs_.get_array().get()));
-
-        auto gkomatrix =
-            gko::share(mtx::create(device_exec, gko::dim<2>(nCells, nCells)));
-
-        SIMPLE_TIME(verbose_, convert_coo_to_csr,
-                    coo_mtx->convert_to(gkomatrix.get());)
-        return gkomatrix;
-    }
-}
-
-
-// void CsrMatrixWrapper::copy_result_back(const scalarField &psi,
-//                                           const label nCells) const
+// template <typename mtx>
+// void MatrixInitFunctor<mtx>::update(std::shared_ptr<dist_mtx> &matrix) const
 // {
-//     // TODO rename to host_x
-//     auto device_x = vec::create(ref_exec(), gko::dim<2>(nCells, 1));
-
-//     std::vector<std::shared_ptr<vec>> device_xs_ptr{};
-//     get_initial_guess(device_xs_ptr);
-//     device_x->copy_from(gko::lend(device_xs_ptr[0]));
-
-//     auto x_view = val_array::view(ref_exec(), nCells,
-//     device_x->get_values());
-//     // for (label i = 0; i < nCells; i++) {
-//     //     std::cout << x_view[i] << std::endl;
-//     // }
-
-//     // move frome device
-//     auto psi_view =
-//         val_array::view(ref_exec(), nCells, const_cast<scalar
-//         *>(&psi[0]));
-
-//     psi_view = x_view;
+//     // auto values_view =
+//     //     val_array::view(values_.get_exec_handler().get_device_exec(),
+//     //                     values_.get_global_size(), matrix->get_values());
+//     // // copy values to device
+//     // values_view = *values_.get_array().get();
 // }
 
-// defineTemplateTypeNameWithName(GKOIDXIOPtr, "IDXIOPtr");
-// defineTemplateTypeNameWithName(GKOCSRIOPtr, "CSRIOPtr");
-// defineTemplateTypeNameWithName(GKOVECIOPtr, "VECIOPtr");
+
+// MatrixInitFunctor<gko::matrix::Csr<double>>::update(std::shared_ptr<dist_mtx>);
+
+// template <typename mtx>
+// std::shared_ptr<dist_mtx> MatrixInitFunctor<mtx>::init() const
+// {
+//     using coo_mtx = gko::matrix::Coo<scalar>;
+
+//     label nCells = partition_.get_local_host_size();
+//     word msg{"init global csr matrix of size " + std::to_string(nCells)};
+//     LOG_1(verbose_, msg)
+
+//     auto values = values_.get_array();
+//     auto cols = col_idxs_.get_array();
+//     auto rows = row_idxs_.get_array();
+
+//     // check if sorted
+//     if (false) {
+//         bool is_sorted_rows = true;
+//         bool is_sorted_cols = true;
+//         auto rows_data = rows->get_const_data();
+//         auto cols_data = cols->get_const_data();
+//         for (size_t i = 1; i < cols->get_num_elems(); i++) {
+//             if (rows_data[i] < rows_data[i - 1]) {
+//                 is_sorted_rows = false;
+//                 Info << "rows sorting error element " << i << " row[i] "
+//                      << rows_data[i] << " row[i-1] " << rows_data[i - 1]
+//                      << endl;
+//             }
+//             // same row but subsequent column is smaller
+//             if (cols_data[i] < cols_data[i - 1] &&
+//                 rows_data[i] == rows_data[i - 1]) {
+//                 is_sorted_cols = false;
+//                 Info << "cols sorting error element " << i << " row[i] "
+//                      << rows_data[i] << " row[i-1] " << rows_data[i - 1]
+//                      << " col[i] " << cols_data[i] << " col[i-1] "
+//                      << cols_data[i - 1] << endl;
+//             }
+//         }
+
+//         Info << "is_sorted rows " << is_sorted_rows << endl;
+//         Info << "is_sorted cols " << is_sorted_cols << endl;
+
+//         if (!is_sorted_cols || !is_sorted_rows) {
+//             for (size_t i = 1; i < cols->get_num_elems(); i++) {
+//                 Info << i << "sparsity (" << rows_data[i] << "," <<
+//                 cols_data[i]
+//                      << ")\n";
+//             }
+//         }
+//     }
+//     auto exec = this->get_exec_handler().get_reference_exec();
+
+//     auto local_mtx =
+//         gko::share(coo_mtx::create(exec, gko::dim<2>(nCells, nCells),
+//                                    *values.get(), *cols.get(), *rows.get()));
+
+
+//     auto local_size = partition_.get_local_host_size();
+
+//     gko::matrix_data<scalar, label> A_data;
+//     local_mtx->write(A_data);
+
+//     auto comm = this->get_exec_handler().get_gko_mpi_comm_wrapper();
+//     auto dist_A = gko::share(dist_mtx::create(*comm.get()));
+//     dist_A->read_distributed(A_data, partition_.get_host_partition().get());
+
+//     if (partition_.get_ranks_per_gpu() == 1) {
+//         return dist_A;
+//     }
+
+//     // TODO test if this needs to be persistent
+//     auto repartitioner =
+//         gko::share(gko::distributed::repartitioner<label, label>::create(
+//             *comm.get(), partition_.get_host_partition(),
+//             partition_.get_device_partition()));
+//     auto to_mat =
+//         gko::share(dist_mtx::create(repartitioner->get_to_communicator()));
+
+//     repartitioner->gather(dist_A.get(), to_mat.get());
+
+//     return to_mat;
+// }
+
+// MatrixInitFunctor<gko::matrix::Csr<double>>::init();
 
 }  // namespace Foam
