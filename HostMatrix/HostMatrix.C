542314d9e73d4c84289e4eb29bdadd35bc20c9ca
Gregor Olenik <gregor.olenik@web.de>
2023-06-20 14:43:48 +0200

    fixup

--- HostMatrix/HostMatrix.C
+++ HostMatrix/HostMatrix.C
@@ -71,13 +71,13 @@ std::vector<scalar> HostMatrixWrapper<MatrixType>::collect_interface_coeffs(
         }
         const auto iface{interfaces.operator()(i)};
         auto coeffs{interfaceBouCoeffs[i]};
-        const label interface_size = face_cells.size();
 
         bool collect = (local)
-                           ? !!isA<processorLduInterface>(iface->interface())
-                           : !isA<processorLduInterface>(iface->interface());
+                           ?!isA<processorLduInterface>(iface->interface())
+                           :!!isA<processorLduInterface>(iface->interface());
 
         if (collect) {
+            const label interface_size = iface->interface().faceCells().size();
             for (label cellI = 0; cellI < interface_size; cellI++) {
                 ret.push_back(coeffs[cellI]);
             }
