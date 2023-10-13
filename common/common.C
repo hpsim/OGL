4e4c2a1b85db0a6506c1f305181f9836ebae096e
Gregor Olenik <gregor.olenik@web.de>
2023-10-13 11:31:27 +0200

    adapt to extend version

--- common/common.C
+++ common/common.C
@@ -88,9 +88,15 @@ void set_gko_solver_property(word sys_matrix_name, const objectRegistry &db,
 {
     const word solvPropsDict = sys_matrix_name + "_gkoSolverProperties";
     if (db.foundObject<regIOobject>(solvPropsDict)) {
+#ifdef WITH_EXTEND_VERSION
+        const_cast<objectRegistry &>(db)
+            .lookupObject<IOdictionary>(solvPropsDict)
+            .set<label>(key, value);
+#else
         const_cast<objectRegistry &>(db)
             .lookupObjectRef<IOdictionary>(solvPropsDict)
             .set<label>(key, value);
+#endif
     } else {
         auto gkoSolverProperties =
             new IOdictionary(IOobject(solvPropsDict, fileName("None"), db,
