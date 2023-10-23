23c1f871f48360e889099d1db3650a589584561c
Gregor Olenik <gregor.olenik@web.de>
2023-10-23 14:31:01 +0200

    consider matrix format for writeout

--- common/common.C
+++ common/common.C
@@ -58,14 +58,24 @@ void export_vec(const word fieldName, const gko::matrix::Dense<scalar> *x,
 }
 
 void export_mtx(const word fieldName, std::shared_ptr<const gko::LinOp> A,
-                const word local, const objectRegistry &db)
+                const word local, const objectRegistry &db, const word matrixFormat)
 {
     std::string folder{db.time().timePath()};
     std::filesystem::create_directories(folder);
     std::string fn{folder + "/" + fieldName + "_A_" + local + ".mtx"};
     std::ofstream stream{fn};
     stream << std::setprecision(15);
-    gko::write(stream, (const gko::matrix::Coo<scalar> *)A.get());
+    switch (matrixFormat){
+        case "Coo":
+            gko::write(stream, (const gko::matrix::Coo<scalar> *)A.get());
+            break;
+        case "Csr":
+            gko::write(stream, (const gko::matrix::Csr<scalar> *)A.get());
+            break;
+        case "Ell":
+            gko::write(stream, (const gko::matrix::Ell<scalar> *)A.get());
+            break;
+    }
 }
 
 void export_system(const word fieldName, const gko::matrix::Csr<scalar> *A,
