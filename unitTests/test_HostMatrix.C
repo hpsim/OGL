60b43231bdef406b2e94006c93892f6e6a7cd357
Gregor Olenik <gregor.olenik@web.de>
2023-12-16 08:06:10 +0100

    format + documentation

--- unitTests/test_HostMatrix.C
+++ unitTests/test_HostMatrix.C
@@ -5,24 +5,26 @@ TEST(HostMatrixConversion, symmetric_update)
 {
     /* test a 5x5 symmetric matrix
      * A =
-     * | 1  1  .  2  .  |
-     * | 1  1  1  .  2  |
-     * | .  1  1  1  .  |
-     * | 2  .  1  1  1  |
-     * | .  2  .  1  1  |
+     * | 1  10  .  20  .  |
+     * | 10  2  11  .  21 |
+     * | .  11   3  12  . |
+     * | 20  .  12  4  13 |
+     * | .  21  .  13   5 |
      */
 
-    std::vector<scalar> d{1., 1., 1., 1., 1.};
-    std::vector<scalar> u{1., 2., 1., 2., 1., 1.};
-    std::vector<label> p{6, 0, 1, 0, 7, 2, 3, 2, 8, 4, 1, 4, 9, 5, 3, 5, 10};
+    std::vector<scalar> d{1., 2., 3., 4., 5.};
+    // for OpenFOAMs addressing see
+    // https://openfoamwiki.net/index.php/OpenFOAM_guide/Matrices_in_OpenFOAM
+    std::vector<scalar> u{10., 11., 20., 12., 21., 13.};
+    std::vector<label> p{6, 0, 2, 0, 7, 1, 4, 1, 8, 3, 2, 3, 9, 5, 4, 5, 10};
 
     label total_nnz{17};
     label upper_nnz{6};
 
     std::vector<scalar> res{0., 0., 0., 0., 0., 0., 0., 0., 0.,
                             0., 0., 0., 0., 0., 0., 0., 0.};
-    std::vector<scalar> exp{1., 1., 2., 1., 1., 1., 2., 1., 1.,
-                            1., 2., 1., 1., 1., 2., 1., 1.};
+    std::vector<scalar> exp{1., 10., 20., 10., 2., 11., 21., 11., 3.,
+                            12., 20., 12., 4., 13., 21., 13., 5.};
 
     Foam::symmetric_update(total_nnz, upper_nnz, p.data(), 1.0, d.data(),
                            u.data(), res.data());
