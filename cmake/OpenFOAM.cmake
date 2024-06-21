5cad3c9377bab45dce42892e1bc043804134a1e9
Gregor Olenik <gregor.olenik@web.de>
2024-06-21 10:33:51 +0200

    add OpenFOAM config

---
+++ cmake/OpenFOAM.cmake
@@ -0,0 +1,25 @@
+# SPDX-FileCopyrightText: 2024 OGL authors
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+add_library(OpenFOAM SHARED IMPORTED)
+
+target_include_directories(
+  OpenFOAM
+  INTERFACE $ENV{FOAM_SRC}/finiteVolume/lnInclude
+            $ENV{FOAM_SRC}/meshTools/lnInclude
+            $ENV{FOAM_SRC}/OpenFOAM/lnInclude
+            $ENV{FOAM_SRC}/OSspecific/POSIX/lnInclude)
+if(APPLE)
+  set_target_properties(
+    OpenFOAM
+    PROPERTIES IMPORTED_LOCATION $ENV{FOAM_LIBBIN}/libOpenFOAM.dylib
+               $ENV{FOAM_LIBBIN}/libfiniteVolume.dylib
+               $ENV{FOAM_LIBBIN}/$ENV{FOAM_MPI}/libPstream.dylib)
+else()
+  set_target_properties(
+    OpenFOAM
+    PROPERTIES IMPORTED_LOCATION $ENV{FOAM_LIBBIN}/libOpenFOAM.so
+               $ENV{FOAM_LIBBIN}/libfiniteVolume.so
+               $ENV{FOAM_LIBBIN}/$ENV{FOAM_MPI}/libPstream.so)
+endif()
