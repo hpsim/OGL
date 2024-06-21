48408080614a79ece3b5edd9d37542ecc23cd8b7
Gregor Olenik <gregor.olenik@web.de>
2024-06-21 10:54:14 +0200

    clean up

---
+++ cmake/ginkgo.cmake
@@ -0,0 +1,17 @@
+# SPDX-FileCopyrightText: 2024 OGL authors
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+include(FetchContent)
+
+if(NOT ${OGL_USE_EXTERNAL_GINKGO})
+    FetchContent_Declare(
+    Ginkgo
+    QUITE
+    GIT_SHALLOW ON
+    GIT_REPOSITORY  "https://github.com/ginkgo-project/ginkgo.git"
+    GIT_TAG ${GINKGO_CHECKOUT_VERSION}
+    )
+
+    FetchContent_MakeAvailable(Ginkgo)
+endif()
