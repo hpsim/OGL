292bf2f910ea85161c6a99a0fd43efc2e924bb0a
Gregor Olenik <gregor.olenik@web.de>
2024-07-01 22:14:17 +0200

    basic unit test

---
+++ unitTests/CommunicationPattern.C
@@ -0,0 +1,14 @@
+// SPDX-FileCopyrightText: 2024 OGL authors
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include "src/CommunicationPattern/CommunicationPattern.H"
+#include "gtest/gtest.h"
+
+TEST(HostMatrixConversion, symmetric_update)
+{
+    std::cout << " out \n"; 
+
+    EXPECT_EQ(0, 0);
+}
+
