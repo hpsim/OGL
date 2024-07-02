a6be8307dd9b3faf2ef3c2e5003b3c252c78c568
Gregor Olenik <gregor.olenik@web.de>
2024-07-02 11:07:23 +0200

    add basic comm pattern unit test

--- unitTests/CommunicationPattern.C
+++ unitTests/CommunicationPattern.C
@@ -4,11 +4,37 @@
 
 #include "src/CommunicationPattern/CommunicationPattern.H"
 #include "gtest/gtest.h"
+#include "mpi.h"
 
-TEST(HostMatrixConversion, symmetric_update)
+class CommunicationPatternFixture : public testing::Test {
+ protected:
+  CommunicationPatternFixture():
+    time(Foam::Time::New()),
+    db(Foam::objectRegistry(*time))
+    {
+    dict.add("executor", "reference");
+  }
+
+  const Foam::Time* time;
+  Foam::objectRegistry db;
+  Foam::dictionary dict;
+};
+
+
+TEST_F(CommunicationPatternFixture, can_get_comm_size)
 {
-    std::cout << " out \n"; 
-
-    EXPECT_EQ(0, 0);
+    auto exec = ExecutorHandler(db, dict, "dummy", true);
+    auto comm = exec.get_gko_mpi_device_comm();
+    EXPECT_EQ(comm->size(), 4);
 }
 
+int main(int argc, char* argv[]) {
+    int result = 0;
+
+    ::testing::InitGoogleTest(&argc, argv);
+    MPI_Init(&argc, &argv);
+    result = RUN_ALL_TESTS();
+    MPI_Finalize();
+
+    return result;
+}
