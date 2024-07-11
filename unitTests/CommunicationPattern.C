579fd13c1d2a98f0276ed7b18c2596e9f4b0513e
chihta-wang <wangchihta@gmail.com>
2024-07-11 14:25:43 +0200

    Adapt the unit test for the function compute_gather_to_owner_counts for the case of only a single owner process so that any number of elements can be set for the test

--- unitTests/CommunicationPattern.C
+++ unitTests/CommunicationPattern.C
@@ -103,25 +103,26 @@ TEST_F(CommunicationPatternFixture, compute_gather_to_owner_counts_single_owner)
 {
     // Arrange
     auto comm = exec->get_gko_mpi_host_comm();
+    auto num_elements = 10;
 
-    // if gathering to just one owner all 10 elements are send to rank 0
+    // if gathering to just one owner, all 10 elements are send to rank 0
     std::vector<std::vector<int>> send_results(comm->size(),
-                                          std::vector<int>{10, 0, 0, 0});
+                                          std::vector<int>{num_elements, 0, 0, 0});
     // no rank should recv anything except rank 0
     std::vector<std::vector<int>> recv_results(comm->size(),
                                           std::vector<int>{0, 0, 0, 0});
-    recv_results[0] = std::vector<int>{10, 10, 10, 10};
+    recv_results[0] = std::vector<int>{num_elements, num_elements, num_elements, num_elements};
 
     std::vector<std::vector<int>> send_offsets_results(comm->size() + 1,
-                                          std::vector<int>{0, 0, 0, 0, 10});
+                                          std::vector<int>{0, 0, 0, 0, num_elements});
 
     std::vector<std::vector<int>> recv_offsets_results(comm->size() + 1,
                                           std::vector<int>{0, 0, 0, 0, 0});
-    recv_offsets_results[0] = std::vector<int>{0, 10, 20, 30, 40};
+    recv_offsets_results[0] = std::vector<int>{0, num_elements, num_elements*2, num_elements*3, num_elements*4};
 
     // Act
     auto comm_counts =
-        compute_gather_to_owner_counts(*exec.get(), comm->size(), label(10));
+        compute_gather_to_owner_counts(*exec.get(), comm->size(), label(num_elements));
 
     // Assert
     // test if the total number of processes is 4, which is hardcoded here
