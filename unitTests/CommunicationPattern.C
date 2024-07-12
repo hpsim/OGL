c547188b14aa101f7bc3e9793a3a64685dfe26b2
chihta-wang <wangchihta@gmail.com>
2024-07-12 14:46:39 +0200

    Add a new unit test for the function "compute_gather_to_owner_counts" for the case of two owner processes

--- unitTests/CommunicationPattern.C
+++ unitTests/CommunicationPattern.C
@@ -108,7 +108,65 @@ TEST_F(CommunicationPatternFixture, compute_gather_to_owner_counts_single_owner)
     EXPECT_EQ(comm_counts.recv_offsets, recv_offsets);
 }
 
-TEST_F(CommunicationPatternFixture, compute_gather_to_owner_counts_all_owner)
+TEST_F(CommunicationPatternFixture, compute_gather_to_owner_counts_two_owners)
+{
+    // Arrange
+    auto comm = exec->get_gko_mpi_host_comm();
+    auto num_elements = 10;
+    auto comm_rank = comm->rank();
+    auto comm_size = comm->size();
+
+    // Expected results for send counts and recv counts
+    std::vector<int> send_counts(comm_size, 0);
+    if (comm_rank < comm_size/2)
+        send_counts[0] = num_elements;
+    else
+        send_counts[comm_size/2] = num_elements;
+    
+    std::vector<int> recv_counts(comm_size, 0);
+    if (comm_rank == 0)
+    {
+        for (int i = 0; i < comm_size/2; i++)
+            recv_counts[i] = num_elements;
+    }
+    else if (comm_rank == comm_size/2)
+    {
+        for (int i = comm_size/2; i < comm_size; i++)
+            recv_counts[i] = num_elements;
+    }        
+    
+    // Expected results for send offsets and recv offsets
+    std::vector<int> send_offsets(comm_size + 1, 0);
+    send_offsets.back() = num_elements;
+
+    std::vector<int> recv_offsets(comm_size + 1, 0);
+    if (comm_rank == 0)
+    {
+        for (int i = 0; i < comm_size/2; i++)
+            recv_offsets[i] = num_elements * i;
+        
+        recv_offsets.back() = num_elements * comm_size/2;
+    }
+    else if (comm_rank == comm_size/2)
+    {
+        for (int i = comm_size/2, j = 0; i < comm_size; i++, j++)
+            recv_offsets[i] = num_elements * j;
+        
+        recv_offsets.back() = num_elements * comm_size/2;
+    }
+
+    // Act
+    auto comm_counts =
+        compute_gather_to_owner_counts(*exec.get(), comm_size/2, label(num_elements));
+
+    // Assert
+    EXPECT_EQ(comm_counts.send_counts, send_counts);
+    EXPECT_EQ(comm_counts.recv_counts, recv_counts);
+    EXPECT_EQ(comm_counts.send_offsets, send_offsets);
+    EXPECT_EQ(comm_counts.recv_offsets, recv_offsets);
+}
+
+TEST_F(CommunicationPatternFixture, compute_gather_to_owner_counts_all_owners)
 {
     // Arrange
     auto comm = exec->get_gko_mpi_host_comm();
