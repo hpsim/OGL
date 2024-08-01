ebf24c05327923fdf8734f42bd1c05bf54307d12
Gregor Olenik <gregor.olenik@web.de>
2024-08-01 14:28:15 +0200

    switch order

--- unitTests/MatrixWrapper/Combination.C
+++ unitTests/MatrixWrapper/Combination.C
@@ -2,20 +2,27 @@
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-
-#include "fvCFD.H"
-
 #include <gtest/gtest.h>
 
 #include "OGL/MatrixWrapper/Combination.H"
 
+#include "fvCFD.H"
 
-
-TEST(Combination, CanCreateCombination)
+TEST(Combination, CanCreateEmptyCombination)
 {
-    auto cmb = Combination<double, int, gko::matrix::Coo>::create(this->exec);
+    auto cmb = Combination<scalar, label, gko::matrix::Coo>::create(this->exec);
 
     ASSERT_EQ(cmb->get_size(), gko::dim<2>(0, 0));
     ASSERT_EQ(cmb->get_coefficients().size(), 0);
     ASSERT_EQ(cmb->get_operators().size(), 0);
+}
+
+TEST(Combination, CanCreateCombinationWithRanges)
+{
+    std::vector<gko::span> ranges {{0, 5}, {5, 6}};
+    auto cmb = Combination<scalar, label, gko::matrix::Coo>::create(this->exec, gko::dim<2>{5,5}, ranges);
+
+    ASSERT_EQ(cmb->get_size(), gko::dim<2>(5, 5));
+    ASSERT_EQ(cmb->get_coefficients().size(), 2);
+    ASSERT_EQ(cmb->get_operators().size(), 2);
 }
