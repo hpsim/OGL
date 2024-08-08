d83d62378a89f2ab67566f01bdd44f9470b300f6
Gregor Olenik <gregor.olenik@web.de>
2024-08-08 14:01:32 +0200

    update combination matrix and unit tests

--- unitTests/MatrixWrapper/Combination.C
+++ unitTests/MatrixWrapper/Combination.C
@@ -10,23 +10,72 @@
 
 #include <ginkgo/ginkgo.hpp>
 
+
 TEST(Combination, CanCreateEmptyCombination)
 {
     auto exec = gko::share(gko::ReferenceExecutor::create());
-    auto cmb = CombinationMatrix<gko::matrix::Coo<scalar>>::create(exec);
+    auto cmb = CombinationMatrix<gko::matrix::Coo<scalar, label>>::create(exec);
 
     ASSERT_EQ(cmb->get_size(), gko::dim<2>(0, 0));
     ASSERT_EQ(cmb->get_coefficients().size(), 0);
     ASSERT_EQ(cmb->get_operators().size(), 0);
 }
 
-TEST(Combination, CanCreateCombinationWithRanges)
+
+TEST(Combination, CanCreateCombinationWithLinOpVector)
 {
+    auto dim = gko::dim<2>{5, 5};
+    gko::matrix_data<double, int> m1(dim, {{0, 0, 2}, {1, 1, 0}, {2, 3, 5}});
+
     auto exec = gko::share(gko::ReferenceExecutor::create());
-    std::vector<gko::span> ranges {{0, 5}, {5, 6}};
-    auto cmb = CombinationMatrix<gko::matrix::Coo<scalar>>::create(exec, gko::dim<2>{5,5}, ranges);
+    auto m1linop =
+        gko::share(gko::matrix::Csr<scalar, label>::create(exec, dim));
+    m1linop->read(m1);
+
+    std::vector<std::shared_ptr<const gko::LinOp>> linops{m1linop, m1linop};
+
+    auto cmb = CombinationMatrix<gko::matrix::Csr<scalar, label>>::create(
+        exec, dim, linops);
 
     ASSERT_EQ(cmb->get_size(), gko::dim<2>(5, 5));
     ASSERT_EQ(cmb->get_coefficients().size(), 2);
     ASSERT_EQ(cmb->get_operators().size(), 2);
+
+    auto b = gko::matrix::Dense<scalar>::create(exec, gko::dim<2>{5, 1});
+    b->fill(1.0);
+    auto x = gko::matrix::Dense<scalar>::create(exec, gko::dim<2>{5, 1});
+    x->fill(0.0);
+    cmb->apply(b, x);
+
+    ASSERT_EQ(x->at(0, 0), 4.0);
+    ASSERT_EQ(x->at(1, 0), 0.0);
+    ASSERT_EQ(x->at(2, 0), 10.0);
+    ASSERT_EQ(x->at(3, 0), 0.0);
+    ASSERT_EQ(x->at(4, 0), 0.0);
+}
+
+TEST(Combination, CanCreateConvertToCsr)
+{
+    auto dim = gko::dim<2>{5, 5};
+    gko::matrix_data<double, int> m1(dim, {{0, 0, 2}, {1, 1, 0}, {2, 3, 5}});
+    gko::matrix_data<double, int> m2(dim, {{3, 3, 1}, {4, 4, 2}});
+
+    auto exec = gko::share(gko::ReferenceExecutor::create());
+    auto m1linop =
+        gko::share(gko::matrix::Csr<scalar, label>::create(exec, dim));
+    m1linop->read(m1);
+
+    auto m2linop =
+        gko::share(gko::matrix::Csr<scalar, label>::create(exec, dim));
+    m2linop->read(m2);
+
+    std::vector<std::shared_ptr<const gko::LinOp>> linops{m1linop, m2linop};
+
+    auto cmb = CombinationMatrix<gko::matrix::Csr<scalar, label>>::create(
+        exec, dim, linops);
+
+    auto out = CombinationMatrix<gko::matrix::Csr<scalar, label>>::create(
+        exec);
+
+    cmb->convert_to(out);
 }
