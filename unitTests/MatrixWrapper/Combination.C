c84ec5eeb9c356ef55165bf945abd917838e81f2
chihta-wang <wangchihta@gmail.com>
2024-08-14 17:06:57 +0200

    - Implement the function move_to COO format for CombinationMatrix class - Add a unit test for that function

--- unitTests/MatrixWrapper/Combination.C
+++ unitTests/MatrixWrapper/Combination.C
@@ -266,3 +266,63 @@ TEST(Combination, CanConvertToCsr)
     ASSERT_EQ(out->get_row_ptrs()[3], 3);
     ASSERT_EQ(out->get_row_ptrs()[4], 5);
 }
+
+TEST(Combination, CanMoveToCoo)
+{
+    // Arrange
+    using ValueType = scalar;
+    using IndexType = label;
+    using InputFormat = gko::matrix::Csr<ValueType, IndexType>;
+    using OutputFormat = gko::matrix::Coo<ValueType, IndexType>;
+
+    int dim_size = 5;
+
+    auto dim = gko::dim<2>{dim_size, dim_size};
+    gko::matrix_data<double, int> m1(dim, {{0, 0, 2}, {1, 1, -3}, {2, 3, 5}});
+    gko::matrix_data<double, int> m2(dim, {{3, 2, 4}, {3, 3, 1}, {4, 4, 2}});
+
+    auto exec = gko::share(gko::ReferenceExecutor::create());
+    auto m1linop =
+        gko::share(InputFormat::create(exec, dim));
+    m1linop->read(m1);
+
+    auto m2linop =
+        gko::share(InputFormat::create(exec, dim));
+    m2linop->read(m2);
+
+    std::vector<std::shared_ptr<const gko::LinOp>> linops{m1linop, m2linop};
+
+    auto cmb = CombinationMatrix<InputFormat>::create(
+        exec, dim, linops);
+
+    auto out = OutputFormat::create(
+        exec);
+
+    // Act
+    cmb->move_to(out.get());
+
+    // Assert
+    // Verify entry values
+    ASSERT_EQ(out->get_values()[0], 2);
+    ASSERT_EQ(out->get_values()[1], -3);
+    ASSERT_EQ(out->get_values()[2], 5);
+    ASSERT_EQ(out->get_values()[3], 4);
+    ASSERT_EQ(out->get_values()[4], 1);
+    ASSERT_EQ(out->get_values()[5], 2);
+
+    // Verify row indices
+    ASSERT_EQ(out->get_row_idxs()[0], 0);
+    ASSERT_EQ(out->get_row_idxs()[1], 1);
+    ASSERT_EQ(out->get_row_idxs()[2], 2);
+    ASSERT_EQ(out->get_row_idxs()[3], 3);
+    ASSERT_EQ(out->get_row_idxs()[4], 3);
+    ASSERT_EQ(out->get_row_idxs()[5], 4);
+
+    // Verify column indices
+    ASSERT_EQ(out->get_col_idxs()[0], 0);
+    ASSERT_EQ(out->get_col_idxs()[1], 1);
+    ASSERT_EQ(out->get_col_idxs()[2], 3);
+    ASSERT_EQ(out->get_col_idxs()[3], 2);
+    ASSERT_EQ(out->get_col_idxs()[4], 3);
+    ASSERT_EQ(out->get_col_idxs()[5], 4);
+}
