ae2d38aeaca2920771fbcc458fa2f85f8c48d063
Gregor Olenik <gregor.olenik@web.de>
2024-08-15 20:28:28 +0200

    start adding repartitioner unit tests

---
+++ unitTests/Repartitioner.C
@@ -0,0 +1,74 @@
+// SPDX-FileCopyrightText: 2024 OGL authors
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include "OGL/CommunicationPattern.H"
+
+#include "gtest/gtest.h"
+
+#include "mpi.h"
+
+#include <cstdlib>
+
+//---------------------------------------------
+// some_header.h
+extern int my_argc;
+extern char **my_argv;
+// eof
+//---------------------------------------------
+
+//---------------------------------------------
+// main.cpp
+int my_argc;
+char **my_argv;
+
+class RepartitionerEnvironment : public testing::Environment {
+public:
+    void SetUp()
+    {
+        args_ = std::make_shared<Foam::argList>(my_argc, my_argv);
+        time = std::make_shared<Foam::Time>("controlDict", *args_.get());
+        db = std::make_shared<Foam::objectRegistry>(*time.get());
+        Foam::dictionary dict;
+        dict.add("executor", "reference");
+        exec = std::make_shared<ExecutorHandler>(time->thisDb(), dict, "dummy",
+                                                 true);
+
+        auto comm = exec->get_gko_mpi_host_comm();
+        if (comm->size() < 2) {
+            std::cout << "At least 2 CPU processes should be used!"
+                      << std::endl;
+            std::abort();
+        }
+        // delete listener on ranks != 0
+        // to clean up output
+        ::testing::TestEventListeners &listeners =
+            ::testing::UnitTest::GetInstance()->listeners();
+        if (comm->rank() != 0) {
+            delete listeners.Release(listeners.default_result_printer());
+        }
+    }
+
+    std::shared_ptr<Foam::argList> args_;
+    std::shared_ptr<const Foam::Time> time;
+    std::shared_ptr<Foam::objectRegistry> db;
+    std::shared_ptr<ExecutorHandler> exec;
+};
+
+const testing::Environment *global_env =
+    AddGlobalTestEnvironment(new CommunicationPatternEnvironment);
+
+
+TEST(CommunicationPattern, can_create_repartitioner)
+{
+    // Arrange
+    auto exec = ((CommunicationPatternEnvironment *)global_env)->exec;
+    auto repartitioner = Repartitioner(0, 1, 0, exec);
+
+    // Act
+
+    // Assert
+    EXPECT_EQ(owner_rank, 0);
+}
+
+
