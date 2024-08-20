b3e4dae421b2322ba14983f2768864bd1b1d0eca
Gregor Olenik <gregor.olenik@web.de>
2024-08-20 14:17:27 +0200

    fix issue with narrowing conversion

--- unitTests/MatrixWrapper/Combination.C
+++ unitTests/MatrixWrapper/Combination.C
@@ -64,15 +64,16 @@ TEST(Combination, CanCreateCombinationMatrixWithSparseRealMatrix)
     using IndexType = label;
     using mtx = gko::matrix::Csr<ValueType, IndexType>;
     using vec = gko::matrix::Dense<ValueType>;
-    
+
     int dim_size = 5;
-    auto dim = gko::dim<2>{dim_size, dim_size};
+    auto dim = gko::dim<2>{static_cast<gko::size_type>(dim_size),
+                           static_cast<gko::size_type>(dim_size)};
 
     auto exec = gko::share(gko::ReferenceExecutor::create());
 
-    auto m1linop = gko::share(
-        gko::read<mtx>(std::ifstream("data/A_sparse.mtx"), exec));
-    
+    auto m1linop =
+        gko::share(gko::read<mtx>(std::ifstream("data/A_sparse.mtx"), exec));
+
     auto b = gko::read<vec>(std::ifstream("data/b.mtx"), exec);
 
     std::vector<std::shared_ptr<const gko::LinOp>> linops{m1linop, m1linop};
@@ -82,12 +83,13 @@ TEST(Combination, CanCreateCombinationMatrixWithSparseRealMatrix)
         exec, dim, linops);
 
     // Assert
-    ASSERT_EQ(cmb->get_size(), gko::dim<2>(dim_size, dim_size));
+    ASSERT_EQ(cmb->get_size(), dim);
     ASSERT_EQ(cmb->get_coefficients().size(), 2);
     ASSERT_EQ(cmb->get_operators().size(), 2);
 
     // Act
-    auto x = gko::matrix::Dense<scalar>::create(exec, gko::dim<2>{dim_size, 1});
+    auto x = gko::matrix::Dense<scalar>::create(
+        exec, gko::dim<2>{static_cast<gko::size_type>(dim_size), 1});
     x->fill(0.0);
     cmb->apply(b, x);
 
@@ -107,36 +109,36 @@ TEST(Combination, CanCreateCombinationMatrixWithOpenFOAMLDUMatrix)
     using IndexType = label;
     using mtx = gko::matrix::Csr<ValueType, IndexType>;
     using vec = gko::matrix::Dense<ValueType>;
-    
+
     int dim_size = 5;
-    auto dim = gko::dim<2>{dim_size, dim_size};
+    auto dim = gko::dim<2>{static_cast<gko::size_type>(dim_size),
+                           static_cast<gko::size_type>(dim_size)};
 
     auto exec = gko::share(gko::ReferenceExecutor::create());
 
-    auto Llinop = gko::share(
-        gko::read<mtx>(std::ifstream("data/L.mtx"), exec));
+    auto Llinop = gko::share(gko::read<mtx>(std::ifstream("data/L.mtx"), exec));
 
-    auto Dlinop = gko::share(
-        gko::read<mtx>(std::ifstream("data/D.mtx"), exec));
-    
-    auto Ulinop = gko::share(
-        gko::read<mtx>(std::ifstream("data/U.mtx"), exec));
+    auto Dlinop = gko::share(gko::read<mtx>(std::ifstream("data/D.mtx"), exec));
+
+    auto Ulinop = gko::share(gko::read<mtx>(std::ifstream("data/U.mtx"), exec));
 
     auto b = gko::read<vec>(std::ifstream("data/b.mtx"), exec);
 
-    std::vector<std::shared_ptr<const gko::LinOp>> linops{Llinop, Dlinop, Ulinop};
+    std::vector<std::shared_ptr<const gko::LinOp>> linops{Llinop, Dlinop,
+                                                          Ulinop};
 
     // Act
     auto cmb = CombinationMatrix<gko::matrix::Csr<scalar, label>>::create(
         exec, dim, linops);
 
     // Assert
-    ASSERT_EQ(cmb->get_size(), gko::dim<2>(dim_size, dim_size));
+    ASSERT_EQ(cmb->get_size(), dim);
     ASSERT_EQ(cmb->get_coefficients().size(), 3);
     ASSERT_EQ(cmb->get_operators().size(), 3);
 
     // Act
-    auto x = gko::matrix::Dense<scalar>::create(exec, gko::dim<2>{dim_size, 1});
+    auto x = gko::matrix::Dense<scalar>::create(
+        exec, gko::dim<2>{static_cast<gko::size_type>(dim_size), 1});
     x->fill(0.0);
     cmb->apply(b, x);
 
@@ -158,23 +160,21 @@ TEST(Combination, CanConvertToCoo)
 
     int dim_size = 5;
 
-    auto dim = gko::dim<2>{dim_size, dim_size};
+    auto dim = gko::dim<2>{static_cast<gko::size_type>(dim_size),
+                           static_cast<gko::size_type>(dim_size)};
     gko::matrix_data<double, int> m1(dim, {{0, 0, 2}, {1, 1, -3}, {2, 3, 5}});
     gko::matrix_data<double, int> m2(dim, {{3, 2, 4}, {3, 3, 1}, {4, 4, 2}});
 
     auto exec = gko::share(gko::ReferenceExecutor::create());
-    auto m1linop =
-        gko::share(InputFormat::create(exec, dim));
+    auto m1linop = gko::share(InputFormat::create(exec, dim));
     m1linop->read(m1);
 
-    auto m2linop =
-        gko::share(InputFormat::create(exec, dim));
+    auto m2linop = gko::share(InputFormat::create(exec, dim));
     m2linop->read(m2);
 
     std::vector<std::shared_ptr<const gko::LinOp>> linops{m1linop, m2linop};
 
-    auto cmb = CombinationMatrix<InputFormat>::create(
-        exec, dim, linops);
+    auto cmb = CombinationMatrix<InputFormat>::create(exec, dim, linops);
 
     auto out = OutputFormat::create(exec);
 
@@ -217,26 +217,23 @@ TEST(Combination, CanConvertToCsr)
 
     int dim_size = 5;
 
-    auto dim = gko::dim<2>{dim_size, dim_size};
+    auto dim = gko::dim<2>{static_cast<gko::size_type>(dim_size),
+                           static_cast<gko::size_type>(dim_size)};
     gko::matrix_data<double, int> m1(dim, {{0, 0, 2}, {1, 1, -3}, {2, 3, 5}});
     gko::matrix_data<double, int> m2(dim, {{3, 2, 4}, {3, 3, 1}, {4, 4, 2}});
 
     auto exec = gko::share(gko::ReferenceExecutor::create());
-    auto m1linop =
-        gko::share(InputFormat::create(exec, dim));
+    auto m1linop = gko::share(InputFormat::create(exec, dim));
     m1linop->read(m1);
 
-    auto m2linop =
-        gko::share(InputFormat::create(exec, dim));
+    auto m2linop = gko::share(InputFormat::create(exec, dim));
     m2linop->read(m2);
 
     std::vector<std::shared_ptr<const gko::LinOp>> linops{m1linop, m2linop};
 
-    auto cmb = CombinationMatrix<InputFormat>::create(
-        exec, dim, linops);
+    auto cmb = CombinationMatrix<InputFormat>::create(exec, dim, linops);
 
-    auto out = OutputFormat::create(
-        exec);
+    auto out = OutputFormat::create(exec);
 
     // Act
     cmb->convert_to(out.get());
@@ -276,26 +273,23 @@ TEST(Combination, CanMoveToCoo)
 
     int dim_size = 5;
 
-    auto dim = gko::dim<2>{dim_size, dim_size};
+    auto dim = gko::dim<2>{static_cast<gko::size_type>(dim_size),
+                           static_cast<gko::size_type>(dim_size)};
     gko::matrix_data<double, int> m1(dim, {{0, 0, 2}, {1, 1, -3}, {2, 3, 5}});
     gko::matrix_data<double, int> m2(dim, {{3, 2, 4}, {3, 3, 1}, {4, 4, 2}});
 
     auto exec = gko::share(gko::ReferenceExecutor::create());
-    auto m1linop =
-        gko::share(InputFormat::create(exec, dim));
+    auto m1linop = gko::share(InputFormat::create(exec, dim));
     m1linop->read(m1);
 
-    auto m2linop =
-        gko::share(InputFormat::create(exec, dim));
+    auto m2linop = gko::share(InputFormat::create(exec, dim));
     m2linop->read(m2);
 
     std::vector<std::shared_ptr<const gko::LinOp>> linops{m1linop, m2linop};
 
-    auto cmb = CombinationMatrix<InputFormat>::create(
-        exec, dim, linops);
+    auto cmb = CombinationMatrix<InputFormat>::create(exec, dim, linops);
 
-    auto out = OutputFormat::create(
-        exec);
+    auto out = OutputFormat::create(exec);
 
     // Act
     cmb->move_to(out.get());
@@ -337,26 +331,23 @@ TEST(Combination, CanMoveToCsr)
 
     int dim_size = 5;
 
-    auto dim = gko::dim<2>{dim_size, dim_size};
+    auto dim = gko::dim<2>{static_cast<gko::size_type>(dim_size),
+                           static_cast<gko::size_type>(dim_size)};
     gko::matrix_data<double, int> m1(dim, {{0, 0, 2}, {1, 1, -3}, {2, 3, 5}});
     gko::matrix_data<double, int> m2(dim, {{3, 2, 4}, {3, 3, 1}, {4, 4, 2}});
 
     auto exec = gko::share(gko::ReferenceExecutor::create());
-    auto m1linop =
-        gko::share(InputFormat::create(exec, dim));
+    auto m1linop = gko::share(InputFormat::create(exec, dim));
     m1linop->read(m1);
 
-    auto m2linop =
-        gko::share(InputFormat::create(exec, dim));
+    auto m2linop = gko::share(InputFormat::create(exec, dim));
     m2linop->read(m2);
 
     std::vector<std::shared_ptr<const gko::LinOp>> linops{m1linop, m2linop};
 
-    auto cmb = CombinationMatrix<InputFormat>::create(
-        exec, dim, linops);
+    auto cmb = CombinationMatrix<InputFormat>::create(exec, dim, linops);
 
-    auto out = OutputFormat::create(
-        exec);
+    auto out = OutputFormat::create(exec);
 
     // Act
     cmb->move_to(out.get());
