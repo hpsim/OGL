124e32423cd88db7e8c9a7adf2e6ee23c6cbaf2b
Gregor Olenik <gregor.olenik@web.de>
2024-08-24 14:30:52 +0200

    add compute_dimensions implementation

--- src/MatrixWrapper/SparsityPattern.C
+++ src/MatrixWrapper/SparsityPattern.C
@@ -6,6 +6,19 @@
 
 namespace Foam {
 
+gko::dim<2> compute_dimensions(const gko::array<label> &rows)
+{
+    // helper function to get the last element of a ginkgo array
+    auto get_back = [](const gko::array<label> &in) {
+        return *(in.get_const_data() + in.get_size());
+    };
+
+    gko::size_type num_rows = get_back(rows) + 1;
+
+    return gko::dim<2>{num_rows, num_rows};
+}
+
+
 void make_ldu_mapping_consecutive(const AllToAllPattern &comm_pattern,
                                   gko::array<label> &ldu_mapping, label rank,
                                   label ranks_per_gpu)
