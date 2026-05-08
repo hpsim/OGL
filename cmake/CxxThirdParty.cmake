1000981e3134ca56610f22ead62873c4c3de93f0
Gregor Olenik <gregor.olenik@web.de>
2026-05-08 12:51:45 +0200

    add license

--- cmake/CxxThirdParty.cmake
+++ cmake/CxxThirdParty.cmake
@@ -4,15 +4,14 @@
 
 # set(FETCHCONTENT_BASE_DIR ${CMAKE_BINARY_DIR}/cmake_packages)
 
-include(cmake/Versions.cmake)
 include(cmake/CPM.cmake)
 
 if(NOT DEFINED OGL_GINKGO_DIR)
-    set(OGL_GINKGO_CHECKOUT_VERSION
-        "ogl_0600_gko190"
-        CACHE STRING "Use specific version of ginkgo")
-    message(STATUS "Using CPM to get Ginkgo ${GINKGO_CHECKOUT_VERSION}")
-    set(OGL_GINKGO_VIA_CPM ON)
+  set(OGL_GINKGO_CHECKOUT_VERSION
+      "ogl_0600_gko190"
+      CACHE STRING "Use specific version of ginkgo")
+  message(STATUS "Using CPM to get Ginkgo ${GINKGO_CHECKOUT_VERSION}")
+  set(OGL_GINKGO_VIA_CPM ON)
 else()
   message(STATUS "using OGL_GINKGO_DIR ${OGL_GINKGO_DIR}")
   add_subdirectory(${OGL_GINKGO_DIR} ${CMAKE_CURRENT_BINARY_DIR}/Ginkgo)
@@ -39,13 +38,13 @@ if(OGL_GINKGO_VIA_CPM)
       "GINKGO_BUILD_TESTS OFF"
       "GINKGO_BUILD_BENCHMARKS OFF"
       "GINKGO_BUILD_EXAMPLES OFF"
-      "GINKGO_BUILD_OMP OFF"
       "GINKGO_ENABLE_HALF OFF"
       "GINKGO_BUILD_MPI ON"
       "GINKGO_BUILD_PAPI_SDE OFF"
+      "GINKGO_ENABLE_BFLOAT16 OFF"
+      "GINKGO_BUILD_OMP ${OGL_BUILD_OMP}"
       "GINKGO_BUILD_CUDA ${OGL_BUILD_CUDA}"
       "GINKGO_BUILD_HIP ${OGL_BUILD_HIP}"
-      "GINKGO_BUILD_SYCL ${OGL_BUILD_SYCL}"
-      )
+      "GINKGO_BUILD_SYCL ${OGL_BUILD_SYCL}")
   endif()
 endif()
